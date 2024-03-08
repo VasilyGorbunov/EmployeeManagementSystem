b4eaf46257d5022689c61be059aa53de726a6410
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-03-08 23:50:56 +0300

    51:10

--- ServerLibrary/Helpers/JwtSection.cs
+++ ServerLibrary/Helpers/JwtSection.cs
@@ -2,5 +2,8 @@
 {
     public class JwtSection
     {
+        public string? Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
     }
 }
--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -1,19 +1,39 @@
 ﻿using BaseLibrary.DTOs;
+using BaseLibrary.Entities;
 using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
 
 namespace ServerLibrary.Repositories.Implementations
 {
-    public class UserAccountRepository : IUserAccount
+    public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
     {
-        public Task<GeneralResponse> CreateAsync(Register user)
+        public async Task<GeneralResponse> CreateAsync(Register user)
         {
-            throw new NotImplementedException();
+            if (user is null) return new GeneralResponse(false, "Model is empty");
+
+            var checkUser = await FindUserByEmail(user.Email);
+            if (checkUser is not null) return new GeneralResponse(false, "User already exists!");
+
+            var applicationUser = await AddToDatabase(new ApplicationUser()
+            {
+                FullName = user.FullName,
+                Email = user.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
+            });
         }
+
+        
 
         public Task<LoginResponse> SignInAssign(Login user)
         {
             throw new NotImplementedException();
         }
+
+        private async Task<ApplicationUser> FindUserByEmail(string userEmail) =>
+            await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(userEmail!.ToLower()));
     }
 }
